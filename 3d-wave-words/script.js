f41945cf5886ac2134a226a26e3a53ba08d36dbb
Artyom Leonov <6leonov@gmail.com>
2026-04-18 20:39:04 +0300

    fest: implement

--- 3d-wave-words/script.js
+++ 3d-wave-words/script.js
@@ -1,1 +1,21 @@
-// Enter your code here
+const CHARS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+
+const container = document.querySelector(".container")
+
+const words = Array.from({ length: 40 * 40 }).map(() => {
+    const word = document.createElement("div")
+    word.className = "word"
+    word.textContent = CHARS.at(Math.floor(Math.random() * CHARS.length))
+    return container.appendChild(word)
+})
+
+function draw(timestamp) {
+    words.forEach((word, index) => {
+        const z = Math.sin(index % 40 / 2 + timestamp / 500) * Math.sin(index / 40 / 2 + timestamp / 500) * 10
+        word.style.transform = `translateZ(${z}px)`
+    })
+
+    requestAnimationFrame(draw)
+}
+
+requestAnimationFrame(draw)
